15424fb1b9b83420d750d33b909d6f404cef139a
Davi Kilmer <davikilmer13@gmail.com>
2026-07-17 07:31:42 -0300

    Stop tracking release zip package

--- commit_push.bat
+++ commit_push.bat
@@ -1,10 +1,13 @@
 @echo off
 cd /d "%~dp0"
+echo ===== Removendo zip do controle de versao =====
+git rm --cached PRADivider.zip
+echo.
 echo ===== Adicionando alteracoes =====
 git add -A
 echo.
 echo ===== Commit =====
-git commit -m "Add English description to metadata.txt for QGIS repo requirements"
+git commit -m "Stop tracking release zip package"
 echo.
 echo ===== Enviando para o GitHub =====
 git push
